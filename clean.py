8cbcc2e6542e5b99a4bac2179ca17b574cd9239d
“LiLuyaogit config --global user.name “LiLuyao <22420584@life.hkbu.edu.hk>
2023-02-22 18:24:39 +0800

    initial commit

--- clean.py
+++ clean.py
@@ -2,8 +2,8 @@ import argparse
 import pandas as pd
 
 arg_parser = argparse.ArgumentParser(
-    prog = 'clean.py',          # program name
-    description = 'The script for data cleansing'       # function description
+    prog = 'clean.py',
+    description = 'The script for data cleansing'
 )
 
 arg_parser.add_argument('args',nargs=3)
@@ -12,8 +12,6 @@ args = arg_parser.parse_args()
 contact_info_file = args.args[0]
 other_info_file = args.args[1]
 output_file = args.args[2]
-
-#print(args)
 
 if not contact_info_file or not other_info_file or not output_file:
     print("No argument found. Attach -h as the argument to view the help info")
@@ -29,3 +27,5 @@ df = pd.merge(contact_info,other_info,left_on="respondent_id",right_on="id")
 df.dropna(inplace=True)
 df.drop(df[df['job'].str.lower().str.contains('insurance')].index,inplace=True)
 df.to_csv(output_file)
+
+print("The shape of new data: ", cleaned.shape)
